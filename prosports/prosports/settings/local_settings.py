70e960167bfa4c95635327646564f1f9bdaa0195
proprioceptor <siddharth.ghumre92@gmail.com>
2018-04-17 15:04:20 +0800

    Upload files
    initial

---
+++ prosports/prosports/settings/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bad',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
